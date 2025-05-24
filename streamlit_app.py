f5e56b5ca13649832585f3fad7e1a2646bef029d
Sukumar-Data-analyst <pendotasukumar2894@gmail.com>
2025-05-24 16:45:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,13 @@
 import streamlit as st
 from bot_engine import get_account_balance, execute_strategy
 
-st.title("CoinDCX Crypto Bot Dashboard")
+st.title("THE-BOT: CoinDCX Crypto Bot Dashboard")
 
 st.header("Your Account Balance")
 balances = get_account_balance()
-
 if balances:
     for item in balances:
-        coin = item["currency"]
-        bal = item["balance"]
-        st.write(f"**{coin}**: {bal}")
+        st.write(f"**{item['currency']}**: {item['balance']}")
 else:
     st.error("Failed to fetch balances.")
 
